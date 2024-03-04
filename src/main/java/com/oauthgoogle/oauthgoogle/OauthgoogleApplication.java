de13e9bd84abc90ed329b2d968123932b55895a6
Antonio Carlos Virissimo <antonio13050@gmail.com>
2024-03-04 15:57:36 -0300

    first commit

---
+++ src/main/java/com/oauthgoogle/oauthgoogle/OauthgoogleApplication.java
@@ -0,0 +1,13 @@
+package com.oauthgoogle.oauthgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthgoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthgoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauthgoogle/oauthgoogle/OauthgoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthgoogle.oauthgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthgoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
